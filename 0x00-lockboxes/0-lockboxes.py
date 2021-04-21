6896be5b823c491cd622cf158675710ba27661bb
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-21 21:45:31 +0000

    second try horrible ineficient but it works

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
-    for i in range(len(boxes)- 1):
-        check = 0
-        print("loop")
-        for j in range(len(boxes[i])):
-            if boxes[i][j] == i + 1:
-                check = 1
-                break
-        if check == 0:
-            return False
-    return True
+    keys = [0]
+    box = len(boxes) - 1
+    while box != 0:
+        for i in set(keys):
+            for j in set(boxes[i]):
+                if j not in set(keys):
+                    keys.append(j)
+        box -= 1
+    if len(set(keys)) == len(boxes):
+        return True
+    else:
+        return False
