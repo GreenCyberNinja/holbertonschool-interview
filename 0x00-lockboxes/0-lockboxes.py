506e18002147bd835f4dc7dff6e3dd62e698dd8d
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-22 01:34:25 +0000

    fixed outside loop

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,7 +5,7 @@
 def canUnlockAll(boxes):
     keys = [0]
     box = len(boxes) - 1
-    while box <= 0:
+    while box >= 0:
         for i in set(keys):
             for j in set(boxes[i]):
                 if j not in set(keys) and j < len(boxes):
