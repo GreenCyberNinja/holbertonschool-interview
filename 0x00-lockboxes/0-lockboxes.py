cb091d990d9ca56fd34fe6a88985d40072c52b67
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-04-22 01:17:35 +0000

    updated take key statement

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,13 +5,13 @@
 def canUnlockAll(boxes):
     keys = [0]
     box = len(boxes) - 1
-    while box != 0:
+    while box <= 0:
         for i in set(keys):
             for j in set(boxes[i]):
-                if j not in set(keys) or j < len(boxes):
+                if j not in set(keys) and j < len(boxes):
                     keys.append(j)
         box -= 1
-    if len(set(keys)) >= len(boxes):
+    if len(set(keys)) == len(boxes):
         return True
     else:
         return False
