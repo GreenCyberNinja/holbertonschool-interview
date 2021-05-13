6d41ae38894a8f16732e0165da270295e0a53c98
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-05-13 16:41:42 -0500

    added documentation and fix some peep8 complaints

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""
+loops through until lowest factor then copies and pastes it
+"""
+
 
 def minOperations(n):
+    """I think this is efficient? famous last words"""
     if n <= 1:
         return 0
     ans = 0
