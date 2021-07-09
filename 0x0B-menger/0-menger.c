9a6187603bdc6be3f673bc29145173e43003d852
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-07-08 19:12:41 -0500

    project directory readme and rough draft

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,21 @@
+#include "menger.h"
+
+/**
+ * menger - prints a 2d menger fractal of inputed level
+ * @level: the level of menger fractal
+*/
+void menger(int level)
+{
+    if (level >= 0)
+    {
+        if (level == 0)
+            printf("#\n");
+            
+        if (level == 1)
+        {
+            printf("###\n
+                    # #\n
+                    ###\n");
+        }
+    }
+}
---
+++ 0x0B-menger/README.md
@@ -0,0 +1,1 @@
+project directory for interview question 0x0B menger
