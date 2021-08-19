253650921b183727d916eff79620e02adfc20af6
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-08-19 08:26:19 -0500

    project directory readme and interview answer

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def rain(walls):
+    if walls == None:
+        return (0)
+    thud = 0
+    smlstWl = 0
+    rnwtr = 0
+    space = 0
+    for wall in walls:
+        if wall != 0 and thud != 0:
+            if wall < smlstWl:
+                smlstWl = wall
+            rnwtr += smlstWl * space
+            space = 0
+            thud = 0
+            smlstWl = 0
+        if wall != 0 and thud == 0:
+            thud = wall
+            if smlstWl == 0:
+                smlstWl = thud
+        if wall == 0 and thud != 0:
+            space += 1
+    return rnwtr
