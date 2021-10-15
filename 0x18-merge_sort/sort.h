92acbd93fa1190c40d1d59d62f697eba0741021d
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-10-14 21:33:52 -0500

    forgot end statement on printmerging method

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -8,6 +8,6 @@
 void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void RecursiveSort(int *ar, size_t size, int *tmp);
-void PrintMerging(int *ar, size_t size, size_t mdl, int *tmp)
+void PrintMerging(int *ar, size_t size, size_t mdl, int *tmp);
 
 #endif /* SORT_H */
