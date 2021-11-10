5b52f6694b168c5cfae46ab6159cd35ed9f64f09
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-11-10 15:45:54 -0600

    changed method of getting x to a dictionary storing if location is not in dict

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -4,21 +4,20 @@
 
 def island_perimeter(grid):
     """finds the perimeter of island in list of lists"""
-    
-    land = 0
-    x = 0
+
+    count = 0
+    land = False
+    x = {}
     y = 0
     for row in grid:
         for i in row:
+            count += 1
             if i == 1:
-                land += 1
-        if land > 1:
-            if land > x or x == 0:
-                x = land
+                land = True
+                if count not in x:
+                    x[count] = 1
+        if land == True:
             y += 1
-            land = 0
-        else:
-            y += land
-            land = 0
-
-    return ((2*x) + (2*y))
+            land = False
+        count = 0
+    return ((2*len(x)) + (2*y))
