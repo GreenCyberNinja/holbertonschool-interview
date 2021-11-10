bb2e9be401e65e7e1e203a0a079cfa220243fe45
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-11-10 15:02:25 -0600

    tweaked the algo to properally perform perimeter equation

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -5,8 +5,18 @@
 def island_perimeter(grid):
     """finds the perimeter of island in list of lists"""
     land = 0
+    x = 0
+    y = 0
     for row in grid:
         for i in row:
             if i == 1:
                 land += 1
-    return ((land + 1) * 2)
+        if land > 1:
+            x += land
+            y += 1
+            land = 0
+        else:
+            y += land
+            land = 0
+
+    return ((2*x) + (2*y))
